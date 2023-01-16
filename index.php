93f6936fcf5820939894fbffe9f68b87a17acb96
Bruno Santana <brunomoraes.bm.bm@gmail.com>
2023-01-16 13:53:56 -0300

    Landing Page de Alta Conversão

---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="pt_BR" data-theme="light">
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet" />
+        <title>Project 06 | Parallax</title>
+        <!----------Tags SEO Google--------->
+        <meta charset="UTF-8"/>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="copyright" content="" />
+        <meta name="google-site-verification" content="...">
+        <meta name="robots" content="index, follow"/>
+        <meta name="googlebot" content=""/>
+        <meta name="google" content="nositelinkssearchbox">
+        <meta name="googlebot" content="notranslate">
+        <meta name="google" content="nopagereadaloud">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/>
+        <meta name="viewport" content="width=device-width"/>
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="author" content="Bruno Santana"/>
+        <meta name="description" content=""/>
+        <meta name="keyowords" content=""/>
+        <!----------Tags SEO Facebook--------->
+        <meta property="og:url"                content="" />
+        <meta property="og:type"               content="landing Page" />
+        <meta property="og:title"              content="" />
+        <meta prHow much does culture influence creative thinking?" />
+        <meta property="og:image"              content="" />
+        <!----------Tags SEO Twitter--------->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="A Twitter for My Sister" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <!----------Links de Style--------->
+        <link href="images//iconBdev.ico" type="image-x/png" rel="shortcut icon"/>
+        <link href="css/template.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <section class="main">
+            <div class="layer"></div>
+            <div class="center">
+                <header>
+                    <div class="icons-social">
+                        <a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="https://instagram.com"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="https://instagram.com"><i class="fa fa-google" aria-hidden="true"></i></a>
+                        <a href="https://instagram.com"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    </div><!---icons-social--->
+                    <div class="clear"></div>
+                    <div class="logo">
+                        Bdeveloper
+                    </div>
+                    <nav class="desktop">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Informações</a></li>
+                            <li><a href="#">Contatos</a></li>
+                        </ul>
+                    </nav><!---desktop--->
+
+                    <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Informações</a></li>
+                            <li><a href="#">Contatos</a></li>
+                        </ul>
+                    </nav><!---mobile--->
+
+                    <div class="clear"></div>
+                </header>
+                <div class="conteudo-header">
+                    <h2>Bem-vindo(a) ao nosso <span>website!</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet olore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit i</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit i</p>
+				    <a href="">Saiba mais!</a>
+                </div><!---conteudo-header-->
+            </div><!---center-->
+        </section><!---main-->
+        <section class="planos">
+                <div class="teste">
+                    <div class="chamada-planos">
+                        <div class="center"> 
+                            <h2>Confira nossos planos</h2>
+                        </div><!---center--->
+                        <section class="box-planos">
+                            <div class="center">
+
+                                <div class="box-planos-single">
+                                    <div class="box-planos-wraper">
+                                        <h3 class="icons-planos"><i class="fa fa-css3" aria-hidden="true"></i></h3>
+                                        <div class="pontos-planos">
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                        </div><!----pontos-planos--->
+                                        <div class="preco">
+                                            <h2>$250.00</h2>
+                                        </div><!----preco--->
+                                    </div><!---box-planos-wraper--->
+                                </div><!---box-planos-single--->
+
+                                <div class="box-planos-single">
+                                    <div class="box-planos-wraper">
+                                        <h3 class="icons-planos"><i class="fa fa-html5" aria-hidden="true"></i></h3>
+                                        <div class="pontos-planos">
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                        </div><!----pontos-planos--->
+                                        <div class="preco">
+                                            <h2>$250.00</h2>
+                                        </div><!----preco--->
+                                    </div><!---box-planos-wraper--->
+                                </div><!---box-planos-single--->
+
+                                <div class="box-planos-single">
+                                    <div class="box-planos-wraper">
+                                        <h3 class="icons-planos"><i class="fa fa-css3" aria-hidden="true"></i></h3>
+                                        <div class="pontos-planos">
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                            <p><i class="fa fa-check-square"></i> Lorem ipsum silor domor</p>
+                                        </div><!----pontos-planos--->
+                                        <div class="preco">
+                                            <h2>$250.00</h2>
+                                        </div><!----preco--->
+                                    </div><!---box-planos-wraper--->
+                                </div><!---box-planos-single--->
+                            </div><!---center---->
+                        </section><!---box-planos-->
+                    </div><!---chamada-planos-->
+                </div>
+        </section><!----planos---->
+        <section class="parallax">
+            <div class="overlay-parallax"></div>
+            <div class="center">
+            <h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex veritatis nihil voluptas ducimus illo optio cum corporis deleniti architecto distinctio suscipit soluta excepturi rerum perspiciatis asperiores, dolor aliquid quas aperiam? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, hic temporibus perferendis autem corrupti voluptatibus reiciendis laudantium aliquam obcaecati, nihil eos recusandae beatae. Nostrum sapiente beatae delectus! Accusantium, exercitationem corrupti.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum earum aliquid enim maiores. Qui aperiam est quia modi nemo repellendus? Ad, explicabo maxime! Fugiat obcaecati sequi odio hic ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quas rem assumenda aut quae magni omnis. Quasi asperiores quibusdam, consequuntur perspiciatis dolor omnis hic nisi. Libero recusandae vero alias sunt?</p>
+            </div><!---center--->
+        </section><!---parallax--->
+
+        <section class="final-section">
+            <div class="center">
+                <div class="w50 contato">
+                    <h2><div class="borda-efeito"></div>Entre em contato!</h2>
+                    <div class="box-sucesso">Sua mensagem foi enviada com sucesso!</div>
+                    <form>
+
+                        <div class="form-group">
+                            <span></span>
+                            <input type="text" name="nome" placeholder="Insira seu nome...">
+                        </div><!---form-group-->
+
+                        <div class="form-group">
+                            <span></span>
+                            <input type="text" name="email" placeholder="Insira seu nome...">
+                        </div><!---form-group-->
+
+                        <div class="form-group">
+                            <span></span>
+                            <input type="text" name="telefone" placeholder="Insira seu nome...">
+                        </div><!---form-group-->
+
+                        <div class="form-group">
+                            <span></span>
+                        <textarea name="mensagem" placeholder="Sua mensagem...."></textarea>
+                        </div><!---form-group--->
+
+                        <input type="submit" name="acao" value="Enviar!">
+
+                    </form>
+                </div><!---contato-->
+                <div class="w50 time">
+                    <h2><div class="borda-efeito"></div>Conheça nosso time!</h2>
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+                            <div class="info-usuario">
+                                <p class="bold">Bruno Santana</p>
+                                <p class="italic">CEO / Bdeveloper</p>
+                            </div><!---info-usuario--->
+                        </div><!----topo-box-usuario-->
+                        <div class="descricao-usuario-box">
+						    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					    </div><!--descricao-usuario-box-->
+                    </div><!---equipe-single--->
+
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+                            <div class="info-usuario">
+                                <p class="bold">Bruno Santana</p>
+                                <p class="italic">CEO / Bdeveloper</p>
+                            </div><!---info-usuario--->
+                        </div><!----topo-box-usuario-->
+                        <div class="descricao-usuario-box">
+						    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					    </div><!--descricao-usuario-box-->
+                    </div><!---equipe-single--->
+
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+                            <div class="info-usuario">
+                                <p class="bold">Bruno Santana</p>
+                                <p class="italic">CEO / Bdeveloper</p>
+                            </div><!---info-usuario--->
+                        </div><!----topo-box-usuario-->
+                        <div class="descricao-usuario-box">
+						    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					    </div><!--descricao-usuario-box-->
+                    </div><!---equipe-single--->
+
+                    <div class="w50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+                            <div class="info-usuario">
+                                <p class="bold">Bruno Santana</p>
+                                <p class="italic">CEO / Bdeveloper</p>
+                            </div><!---info-usuario--->
+                        </div><!----topo-box-usuario-->
+                        <div class="descricao-usuario-box">
+						    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit</p>
+					    </div><!--descricao-usuario-box-->
+                    </div><!---equipe-single--->
+                    <div class="clear"></div>
+                </div><!---time--->
+                <div class="clear"></div>
+            </div><!---center--->
+        </section><!---final-section-->
+        <footer>
+		<p>Todos os direitos reservados ao Projeto #6!</p>
+	    </footer>
+        <script src="js/jquery-3.6.3.min.js"></script>
+        <!--<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>-->
+        <script>
+            $(function(){
+                $('form').submit(function(){
+                    var erro = false;
+                    var nome = $('[name=nome]').val();
+                    var email = $('[name=email]').val();
+                    var telefone = $('[name=telefone]').val();
+                    var mensagem = $('[name=mensagem]').val();
+                    resetForm();
+                    if(nome == ''){
+                        erro = true;
+                        var el = $('[name=nome]');
+                        el.parent().find('span').html('Por favor insira seu nome.');
+                        el.css('border','2px solid red');
+                    }
+                    if(email == ''){
+                        erro = true;
+                        var el = $('[name=email]');
+                        el.parent().find('span').html('Por favor insira seu e-mail.');
+                        el.css('border','2px solid red');
+                    }
+                    if(telefone == ''){
+                        erro = true;
+                        var el = $('[name=telefone]');
+                        el.parent().find('span').html('Por favor insira seu telefone.');
+                        el.css('border','2px solid red');
+                    }
+                    if(mensagem == ''){
+                        erro = true;
+                        var el = $('[name=mensagem]');
+                        el.parent().find('span').html('Por favor insira seu mensagem.');
+                        el.css('border','2px solid red');
+                    }
+
+                    if(erro == false){
+                        $('.box-sucesso').fadeIn(function(){
+                            setTimeout(function(){
+                                $('.box-sucesso').fadeOut();
+                            },5000);
+                        });
+                        $(this)[0].reset();
+                    }
+
+                    return false;
+                })
+
+                function resetForm(){
+                    $('input[type=text],input[type=email],textarea').css('border','1px solid #ccc');
+                    $('span').html('');
+                }
+            })
+        </script>
+        <script>
+            /****MENU MOBILE****/
+            $(function(){
+                $('nav.mobile i').click(function(){
+                    var el = $(this).parent().find('ul');
+                    if(el.is(':visible') == false) {
+                        el.fadeIn();
+                    }else {
+                        el.fadeOut();
+                    }
+                })
+            })
+        </script>
+        
+    </body>
+    </html>
